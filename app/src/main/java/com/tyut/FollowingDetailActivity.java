6bc517f01586144dcb586b07830c2911250b42ee
vegeta <real685@163.com>
2020-01-29 21:51:39 +0800

    first change test

--- app/src/main/java/com/tyut/FollowingDetailActivity.java
+++ app/src/main/java/com/tyut/FollowingDetailActivity.java
@@ -39,6 +39,8 @@ public class FollowingDetailActivity extends AppCompatActivity implements OnClic
     LinearLayout return_ll;
     Button follow_btn;
 
+    //第一次修改
+
     private static final int INITNAMEANDPIC = 0;
     private static final int INITFOLLOWERCOUNT = 1;
     private static final int INITFOLLOWINGCOUNT = 2;
